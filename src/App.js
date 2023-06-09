f5ff877ea853bb70872c3fd76304b64d64a567fc
aakarshan-raj <aakarshan7raj@gmail.com>
2023-06-09 12:47:32 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
@@ -9,20 +9,7 @@ const App = () => {
   const [keysPressed, setKeysPressed] = useState(0);
   const [mistakes, setMistakes] = useState(0);
   const [accuracy, setAccuracy] = useState(100);
-  const [timer, setTimer] = useState(300);
-  const [intervalId, setIntervalId] = useState(null);
-
-  useEffect(() => {
-    if (timer > 0) {
-      const id = setTimeout(() => {
-        setTimer((prevTimer) => prevTimer - 1);
-      }, 1000);
-
-      setIntervalId(id);
-    } else {
-      clearInterval(intervalId);
-    }
-  }, [timer]);
+  const [timer, setTimer] = useState(0);
 
   const getRandomKey = () => {
     const randomIndex = Math.floor(Math.random() * keys.length);
@@ -38,6 +25,15 @@ const App = () => {
       setKeysPressed((prevKeysPressed) => prevKeysPressed + 1);
     } else {
       setMistakes((prevMistakes) => prevMistakes + 1);
+      setKeysPressed((prevKeysPressed) => prevKeysPressed + 1);
+
+    }
+
+    if (timer === 0) {
+      setTimer(300);
+      setInterval(() => {
+        setTimer((prevTimer) => prevTimer - 1);
+      }, 1000);
     }
   };
 
@@ -57,43 +53,44 @@ const App = () => {
 
   return (
     <div>
-    <h1 className="title">TOUCH TYPING PRACTICE</h1>
+      <h1 className="title">TOUCH TYPING PRACTICE</h1>
 
-    <div className="container">
-      <div className="info-container">
-        <div className="info">
-          <span className="label">Time Remaining:</span>
-          <span className="value">{timer >= 0 ? timer : 0} seconds</span>
+      <div className="container">
+        <div className="info-container">
+          <div className="info">
+            <span className="label">Time Remaining:</span>
+            <span className="value">{timer >= 0 ? timer : 0} seconds</span>
+          </div>
+          <div className="info">
+            <span className="label">Keys Pressed:</span>
+            <span className="value">{keysPressed}</span>
+          </div>
+          <div className="info">
+            <span className="label">Accuracy:</span>
+            <span className="value">{accuracy}%</span>
+          </div>
         </div>
-        <div className="info">
-          <span className="label">Keys Pressed:</span>
-          <span className="value">{keysPressed}</span>
+        <div className="key-info">
+          <span className="label">Next Key:</span>
+          <span className="value">{nextKey}</span>
         </div>
-        <div className="info">
-          <span className="label">Accuracy:</span>
-          <span className="value">{accuracy}%</span>
+        <div className="key-info">
+          <span className="label">Current Key:</span>
+          <span className="value">{currentKey}</span>
+        </div>
+        <input
+        placeholder='Type To Start Counter'
+          type="text"
+          className="typing-box"
+          value={currentKey}
+          onChange={() => {}}
+          onKeyDown={handleKeyPress}
+          autoFocus={true}
+        />
+        <div className="reset-button">
+          <button onClick={() => window.location.reload()}>RESET</button>
         </div>
       </div>
-      <div className="key-info">
-        <span className="label">Next Key:</span>
-        <span className="value">{nextKey}</span>
-      </div>
-      <div className="key-info">
-        <span className="label">Current Key:</span>
-        <span className="value">{currentKey}</span>
-      </div>
-      <input
-        type="text"
-        className="typing-box"
-        value={currentKey}
-        onChange={() => {}}
-        onKeyDown={handleKeyPress}
-        autoFocus={true}
-      />
-       <div className="reset-button">
-      <button onClick={() => window.location.reload()}>{'Reset'}</button>
-    </div>
-    </div>
     </div>
   );
 };
